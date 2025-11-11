44d058154c787c50a0dd3efea09fe8323ab8c649
Julia <juuliacristiny@gmail.com>
2025-11-11 11:43:52 -0300

    operadores relacionais

---
+++ OperadoresRelacionais.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// Comparar variaveis e tomar decisões importantes dentro do codigo
+
+int main() {
+
+int a = 10;
+int b = 20;
+
+printf("a > b: %d\n", a > b);
+printf("a < b: %d\n", a < b);
+printf("a == b: %d\n", a == b);
+printf("a != b: %d\n", a != b);
+return 0;
+}
+ 
