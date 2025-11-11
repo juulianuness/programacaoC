90cacfcca1d073cf88b926a89161e70cce85eb47
Julia <juuliacristiny@gmail.com>
2025-11-11 11:49:29 -0300

    operadores relacionais

--- NivelAvançado/OperadoresRelacionais.c
+++ NivelAvançado/OperadoresRelacionais.c
@@ -10,6 +10,15 @@ printf("a > b: %d\n", a > b);
 printf("a < b: %d\n", a < b);
 printf("a == b: %d\n", a == b);
 printf("a != b: %d\n", a != b);
+
+//conversão explicita 
+
+float numero1 = 10.2;
+int numero2 = 10;
+
+printf("numero1 > numero2: %d\n", (int)numero1 > numero2); //o numero1 é convertido para int antes da comparação
+printf("numerol numero2: %d\n", numero1 == numero2);
+
 return 0;
 }
  
